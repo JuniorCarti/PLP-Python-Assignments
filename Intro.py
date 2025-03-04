c708addaf3e36fc97521e7aca368159eeaa97adf
JuniorCarti <ridgejunior204@gmail.com>
2025-03-04 15:24:10 +0300

    Python Module Classes

---
+++ Intro.py
@@ -0,0 +1,13 @@
+
+
+#variables that hold the data
+age = 30 #integer
+height = 6.75 #float for decimals
+name = "Ridge" #string
+is_student = True #boolean
+
+#printing out the values
+print("Name:, name")
+print("Age:", age)
+print("Height:", height)
+print("Is a student?", is_student)
---
+++ Intro2.py
@@ -0,0 +1,11 @@
+#Integer (whole number)
+age = 30 #integer
+height = 5.668 #float is a decimal 
+name = "Ridge" #String is always a text
+Student = True #a boolean is a true or false statement
+
+#check data types and print the outputs
+print (type(age))    #output should be an integer
+print (type(height))  #output should be a float
+print (type(name))  #output should be a string
+print (type(Student))  #output should be a boolean true or false statement
