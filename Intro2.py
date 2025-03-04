950630d1d14f0e3a7f11d0eb1db2ce79feeb0b90
JuniorCarti <ridgejunior204@gmail.com>
2025-03-04 15:58:46 +0300

    I have added A few modules and Functions on Python

--- Intro2.py
+++ Intro2.py
@@ -9,3 +9,6 @@ print (type(age))    #output should be an integer
 print (type(height))  #output should be a float
 print (type(name))  #output should be a string
 print (type(Student))  #output should be a boolean true or false statement
+
+from Greetings import greet
+greet("Ridge")
---
+++ MathModule.py
@@ -0,0 +1,4 @@
+import math
+
+# Using the math module to calculate the square root
+print(math.sqrt(16))  # Outputs: 4.0
